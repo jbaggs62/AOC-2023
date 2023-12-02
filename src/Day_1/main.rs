435b291c09a7b609cc22ec4b44cf02f798e397a7
jacob baggs <jmb@pop-os.localdomain>
2023-12-02 15:12:01 -0800

    stuck on part 2

--- src/Day_1/main.rs
+++ src/Day_1/main.rs
@@ -1,71 +1,64 @@
-use std::{
-    fs::File,
-    io::{prelude::*, BufReader},
-    path::Path
-};
+use std::collections::HashMap;
 
 fn main()
 {
-    let lines_of_text = lines_from_file("src/Day_1/input.txt");
-    let clean_strings = processor(lines_of_text);
-    let combined_strings = convert_to_integers(clean_strings);
-    let total = sum_values(combined_strings);
-    println!("Sum: {}", total)
-
+   println!("Hello-World");
+   processor();
+   processor_pt2();
 }
 
-fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
-    let file = File::open(filename).expect("no such file");
-    let buf = BufReader::new(file);
-    buf.lines()
-        .map(|l| l.expect("Could not parse line"))
-        .collect()
-}
+const INPUT: &str = include_str!("input.txt");
+fn processor (){
+    let digits: Vec<String> = INPUT.lines().map(|x| x.chars().filter(|x| x.is_numeric()).collect()).collect();
 
-fn processor(input: Vec<String>) -> Vec<Vec<char>> {
-    let mut result: Vec<Vec<char>> = Vec::new();
-
-    for string in input {
-        let mut filtered_chars: Vec<char> = Vec::new();
-
-        for char in string.chars() {
-            if char.is_digit(10) {
-                filtered_chars.push(char);
-            }
-        }
-
-        let mut first_last: Vec<char> = Vec::new();
-        if let Some(first) = filtered_chars.first().cloned() {
-            first_last.push(first);
-            if filtered_chars.len() > 1 {
-                if let Some(last) = filtered_chars.last().cloned() {
-                    first_last.push(last);
-                }
-            }
-        }
-
-        result.push(first_last); // Store the first and last characters in the result
-    }
-
-    result
-}
-
-fn convert_to_integers(input: Vec<Vec<char>>) -> Vec<i32> {
-    let mut result: Vec<i32> = Vec::new();
-
-    for chars in input {
-        let string: String = chars.into_iter().collect();
-        if let Ok(number) = string.parse::<i32>() {
-            result.push(number);
+    let mut sum = 0;
+    for digit in digits {
+        let characters: Vec<char> = digit.chars().collect();
+        if characters.len() >= 1 {
+            sum += format!("{}{}", characters[0], characters[characters.len() -1]).parse::<i32>().unwrap();
         }
     }
 
-    result
-}
-fn sum_values(values: Vec<i32>) -> i32 {
-    values.iter().sum()
+    println!("{}", sum)
 }
 
+
+
+
+fn processor_pt2() {
+    let mut number_map = HashMap::new();
+    number_map.insert("one", 1);
+    number_map.insert("two", 2);
+    number_map.insert("three", 3);
+    number_map.insert("four", 4);
+    number_map.insert("five", 5);
+    number_map.insert("six", 6);
+    number_map.insert("seven", 7);
+    number_map.insert("eight", 8);
+    number_map.insert("nine", 9);
+
+    const INPUT: &str = include_str!("input.txt");
+
+    let mut replaced_string = String::from(INPUT);
+
+    for (word, &val) in &number_map {
+        replaced_string = replaced_string.replace(word, &val.to_string());
+    }
+
+    let digits: Vec<&str> = replaced_string.split_whitespace().collect(); // Splitting the replaced string into individual words
+
+    let mut sum = 0;
+    for digit in &digits {
+        let characters: Vec<char> = digit.chars().collect();
+        if characters.len() >= 1 {
+            sum += format!("{}{}", characters[0], characters[characters.len() - 1])
+                .parse::<i32>()
+                .unwrap_or(0); // Handling if parsing fails
+        }
+    }
+
+    println!("Sum of parsed digits: {}", sum);
+}
 
 
 
